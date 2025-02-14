79695bee6a9bfaacaa106db3e4771c1a65bbbfd5
Kellar <kellar.base09@gmail.com>
2025-02-14 17:40:42 +0000

    attempting to deploy still

--- deploy/core/021_deploy_SimpleAccountFactory.ts
+++ deploy/core/021_deploy_SimpleAccountFactory.ts
@@ -1,11 +1,19 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
-import { deployNonUpgradeable } from '../helpers/deployNonUpgradeable';
 
-const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  await deployNonUpgradeable(hre, 'SimpleAccountFactory', [
-    '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789',
-  ]);
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy('SimpleAccountFactory', {
+    from: deployer,
+    args: ['0x0000000071727de22e5e9d8baf0edac6f37da032'],
+    log: true,
+    gasLimit: 6e6,
+    deterministicDeployment: true,
+  });
 };
 
 export default func;
+func.tags = ['SimpleAccountFactory'];
